b14367f0e071ab6a7ef6b8f0c022e1b525a80e6e
Eudyx <47795745+Eudyx@users.noreply.github.com>
2023-06-27 20:03:48 -0400

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://app-de-turnos.onrender.com'
+    baseURL: 'https://app-de-turnos.onrender.com/'
     // baseURL: 'http://localhost:3000/'
 })
